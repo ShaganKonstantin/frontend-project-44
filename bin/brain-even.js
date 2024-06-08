7ab85e92602fad18f8a3a6bd802d9c4fb823efec
Konstantin Shagan <konstantinshagan@gmail.com>
2024-06-08 16:55:20 +0300

    fixed to be 3 wins straight

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,37 +7,39 @@ console.log(`Hello, ${name}!`);
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const playBrainEven = () => {
+  const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-let attemptsCount = 0;
-let correctAnswerCount = 0;
+  let attemptsCount = 0;
+  let correctAnswerCount = 0;
 
-while (correctAnswerCount < 3 && attemptsCount < 3) {
-  const randomNumber = getRandomNumber(0, 50);
-  console.log(`Question: ${randomNumber}`);
+  while (correctAnswerCount < 3 && attemptsCount < 3) {
+    const randomNumber = getRandomNumber(0, 50);
+    console.log(`Question: ${randomNumber}`);
 
-  const reply = readlineSync.question('Your answer: ');
+    const reply = readlineSync.question('Your answer: ');
 
-  if (randomNumber % 2 === 0 && reply === 'yes') {
-    console.log('Correct!');
-    correctAnswerCount += 1;
-  } else if (randomNumber % 2 === 0 && reply === 'no') {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-    console.log(`Let's try again, ${name}`);
-    attemptsCount += 1;
-  } else if (randomNumber % 2 !== 0 && reply === 'no') {
-    console.log('Correct!');
-    correctAnswerCount += 1;
-  } else if (randomNumber % 2 !== 0 && reply === 'yes') {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${name}`);
-    attemptsCount += 1;
-  } else if (reply !== 'yes' && reply !== 'no') {
-    console.log(`${reply} is wrong answer, try again`);
-    attemptsCount += 1;
+    if (randomNumber % 2 === 0 && reply === 'yes') {
+      console.log('Correct!');
+      correctAnswerCount += 1;
+    } else if (randomNumber % 2 === 0 && reply === 'no') {
+      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${name}`);
+      correctAnswerCount = 0;
+    } else if (randomNumber % 2 !== 0 && reply === 'no') {
+      console.log('Correct!');
+      correctAnswerCount += 1;
+    } else if (randomNumber % 2 !== 0 && reply === 'yes') {
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}`);
+      correctAnswerCount = 0;
+    } else if (reply !== 'yes' && reply !== 'no') {
+      console.log(`${reply} is wrong answer, try again`);
+      correctAnswerCount = 0;
+    }
+  }
+
+  if (correctAnswerCount === 3) {
+    console.log(`Congratulations, ${name}`);
   }
 };
 
-if (correctAnswerCount === 3) {
-  console.log(`Congratulations, ${name}`);
-}
+playBrainEven();
